7ee9488e2b21fb2149c0ac28a4ef0a899f62d2a9
Keyur Raval <ravalkeyur054@gmail.com>
2022-10-27 23:53:34 +0530

    Grid added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { Grid } from '@mui/material';
 import './App.css';
 import Index from './components/Header/Index';
 import JobCard from './components/Job/JobCard';
+import NewJobModal from './components/Job/NewJobModal';
 import SearchBar from './components/SearchBar/SearchBar';
+import jobData from './dummyData'
 import theme from './theme/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Index />
+      <NewJobModal />
       <Grid container justifyContent="center">
         <Grid item xs={10}>
           <SearchBar />
-          <JobCard />
-
+          {jobData.map(job => <JobCard key={job.id}{...job} />)}
         </Grid>
       </Grid>
 
